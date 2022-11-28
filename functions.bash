687e39e5fa028d787f4184cee0690d2bf8118f31
たなかともあき <86752616+vtj-ttanaka@users.noreply.github.com>
2022-11-28 17:27:23 +0900

    HELM_CONFIG_HOMEを渡せるようにした

--- functions.bash
+++ functions.bash
@@ -42,11 +42,20 @@ aws() {
 }
 
 helm() {
+    if [ -z "$HELM_CONFIG_HOME" ] && [ -x "$(which helm)" ]; then
+        eval "$(command helm env | grep '^HELM_CONFIG_HOME=')"
+    fi
+
+    # HELM_CONFIG_HOMEをセットしていなくて、helmコマンドがインストールされていない場合は~/.config/helmを強制
+    local helm_config_home="${HELM_CONFIG_HOME:-${HOME}/.config/helm}"
+    install -Dd "$helm_config_home"
+
     docker run --rm \
            -w /workdir \
            -v $(pwd):/workdir \
            -v ${HOME}/.aws:/root/.aws \
            -v $(dirname ${KUBECONFIG:-${HOME}/.kube/config}):/root/.kube \
+           -v "$helm_config_home:/root/.config/helm" \
            --env AWS_PROFILE \
            --env AWS_ACCESS_KEY_ID \
            --env AWS_SECRET_ACCESS_KEY \
